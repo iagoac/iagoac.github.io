e48aa35ba49503f3e2b1db22f1017ed4961b3af8
Iago Carvalho <iagoac@Iagos-MacBook-Pro.local>
2017-08-30 17:55:45 -0300

    Rodapé arrumado (só no index.html)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,5 @@
 
-jQuery(document).ready(function() {
-
-	$(function(){
-    var includes = $('[data-include]');
-    jQuery.each(includes, function(){
-      var file = 'views/' + $(this).data('include') + '.html';
-      $(this).load(file);
-    });
-  });
-	
+jQuery(document).ready(function() {	
 	/*
 	    Wow
 	*/
